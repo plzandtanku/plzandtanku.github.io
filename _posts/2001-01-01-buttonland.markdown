e3551a3163ff61161929576edd394e14b2cdfe1a
plzandtanku <mrkennethliu@gmail.com>
2019-02-04 18:50:49 -0800

    [buttonland stuff]

--- _posts/2001-01-01-buttonland.markdown
+++ _posts/2001-01-01-buttonland.markdown
@@ -5,6 +5,10 @@ date:   2016-05-14 20:35:49 -0400
 categories: jekyll update
 ---
 
-<button>hi</button>
+<div id="board">
+</div>
+<script>
+</script>
+<button onclick="spawn();">hi</button>
 <button>no</button>
 
--- js/buttonland.js
+++ js/buttonland.js
@@ -1,3 +1,13 @@
+function makeGrid(id){
+	var table = document.getElementById(id);
+	for (var i=0;i < 100;i++){
+		var row = table.insertRow(0);
+		for (var j=0;j<100;j++){
+			var col = row.insertCell(0);
+			col.innerHTML = "<div id="+i+","+j+"> </div>";
+		}
+	}
+}
 
 function spawn(){
 	let buttons = document.getElementsByTagName('button');
